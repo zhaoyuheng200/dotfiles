8470043d99ac5dd0b465f2afd537aa7e24d9c23e
Neil Zhao <neilzhao200@gmail.com>
2024-09-20 18:30:36 -0400

    p1 changes

--- nvim/lua/neil/init.lua
+++
@@ -1,8 +0,0 @@
-require("catppuccin").setup({
-  flavour = "macchiato",
-})
-vim.cmd.colorscheme("catppuccin-mocha")
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.bo.softtabstop = 2
