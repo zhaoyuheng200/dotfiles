f9d51ced2774be71fdbb7e34802a72cfed714f9c
Neil Zhao <neilzhao200@gmail.com>
2024-09-23 01:05:30 -0400

    update

---
+++ nvim/lua/plugins/neotest.lua
@@ -0,0 +1,9 @@
+return {}
+-- return {
+--   "nvim-neotest/neotest",
+--   dependencies = {
+--     "nvim-neotest/nvim-nio",
+--     "nvim-lua/plenary.nvim",
+--     "nvim-treesitter/nvim-treesitter",
+--   },
+-- }
