412737a25d50b4f334ba2ddaad39b25cfcd126c3
Neil Zhao <neilzhao@amazon.com>
2024-12-03 19:04:27 +0000

    chore: update and use snack instead of mini

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---vim.g.lazyvim_picker = "telescope"
+-- vim.g.lazyvim_picker = "telescope"
 vim.g.lazyvim_picker = "fzf"
-vim.g.lazyvim_python_lsp = "python-lsp-server"
+-- vim.g.lazyvim_python_lsp = "python-lsp-server"
+-- vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.lazyvim_python_lsp = "pyright"
